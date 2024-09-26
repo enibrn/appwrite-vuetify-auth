e218ab1dd1d85ad9d7d85e54bd531abfbf454930
Andrea Binelli <binelli.andrea@gmail.com>
2024-09-26 23:35:09 +0200

    user in session

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,26 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-alert v-if="loggedInUser" type="success">
-          Logged in as {{ loggedInUser.name }}
+        <v-alert
+          v-if="user"
+          type="success"
+        >
+          Logged in as {{ user.name }}
         </v-alert>
-        <v-alert v-else type="error">
+        <v-alert
+          v-else
+          type="error"
+        >
           Not logged in
         </v-alert>
       </v-col>
     </v-row>
     <v-form>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col
+          cols="12"
+          md="4"
+        >
           <v-text-field
             label="Email"
             v-model="email"
@@ -20,7 +29,10 @@
             placeholder="Email"
           ></v-text-field>
         </v-col>
-        <v-col cols="12" md="4">
+        <v-col
+          cols="12"
+          md="4"
+        >
           <v-text-field
             label="Password"
             v-model="password"
@@ -28,7 +40,10 @@
             placeholder="Password"
           ></v-text-field>
         </v-col>
-        <v-col cols="12" md="4">
+        <v-col
+          cols="12"
+          md="4"
+        >
           <v-text-field
             label="Name"
             v-model="name"
@@ -37,15 +52,33 @@
           ></v-text-field>
         </v-col>
       </v-row>
-      <v-row>
+      <v-row align="center" justify="center">
         <v-col>
-          <v-btn @click="login(email, password)" color="primary">
+          <v-btn
+            :disabled="user"
+            @click="login(email, password)"
+            color="primary"
+          >
             Login
           </v-btn>
-          <v-btn @click="register" color="secondary">
+        </v-col>
+
+        <v-col>
+          <v-btn
+            :disabled="user"
+            @click="register"
+            color="secondary"
+          >
             Register
           </v-btn>
-          <v-btn @click="logout" color="error">
+        </v-col>
+
+        <v-col>
+          <v-btn
+            :disabled="!user"
+            @click="logout"
+            color="error"
+          >
             Logout
           </v-btn>
         </v-col>
@@ -55,17 +88,21 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { account, ID } from './lib/appwrite.js';
+import { ref, onMounted } from 'vue';
+import useAppwrite from './composables/useAppwrite.js';
 
-const loggedInUser = ref(null);
+const { account, user, ID, evaluateUser } = useAppwrite();
+
 const email = ref('');
 const password = ref('');
 const name = ref('');
 
+onMounted(() => {
+  evaluateUser();
+})
+
 const login = async (email, password) => {
   await account.createEmailPasswordSession(email, password);
-  loggedInUser.value = await account.get();
 };
 
 const register = async () => {
@@ -75,6 +112,5 @@ const register = async () => {
 
 const logout = async () => {
   await account.deleteSession('current');
-  loggedInUser.value = null;
 };
 </script>
---
+++ src/composables/useAppwrite.js
@@ -0,0 +1,45 @@
+import { Client, Account, ID } from 'appwrite';
+import { ref } from 'vue';
+
+const user = ref(null);
+
+const sessionIsDeleted = (response) => {
+    return response.events &&
+        response.events.length >= 2 &&
+        response.events[1] === 'users.*.sessions.*.delete';
+}
+
+const evaluateUser = async () => {
+    console.log("eval user");
+    console.log("was", user);
+    try {
+        user.value = await account.get();
+    } catch (error) {
+        user.value = null;
+    }
+    console.log("now is", user);
+};
+
+const client = new Client();
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(import.meta.env.VITE_APPWRITE_KEY);
+
+const account = new Account(client);
+// console.log("eval user fist time");
+// await evaluateUser();
+
+client.subscribe('account', async response => {
+    console.log(response);
+
+    if (sessionIsDeleted(response)) {
+        user.value = null;
+        return;
+    }
+
+    await evaluateUser();
+});
+
+export default function useAppwrite() {
+    return { account, user, ID, evaluateUser };
+}
